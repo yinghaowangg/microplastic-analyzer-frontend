e6b4a6aa1b761e56a00ecbf889a9e62772132c3e
Wang yinghao <yinghaow@tcd.ie>
2025-09-13 01:22:37 +0100

    Fix Router basename for GitHub Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UploadPage from "./pages/UploadPage";
 import ReportPage from "./pages/ReportPage";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<UploadPage />} />
-      <Route path="/report" element={<ReportPage />} />
-    </Routes>
+    
+    <BrowserRouter basename="/microplastic-analyzer-frontend">
+      <Routes>
+        <Route path="/" element={<UploadPage />} />
+        <Route path="/report" element={<ReportPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
