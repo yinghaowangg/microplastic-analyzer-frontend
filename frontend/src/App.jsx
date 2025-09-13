b8f64e3bb59961b46c4b3d9ebee7419d7df6b00c
Wang yinghao <yinghaow@tcd.ie>
2025-09-13 02:04:55 +0100

    Fix double Router issue by moving BrowserRouter to main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import UploadPage from "./pages/UploadPage";
 import ReportPage from "./pages/ReportPage";
 
 export default function App() {
   return (
-    
-    <BrowserRouter basename="/microplastic-analyzer-frontend">
-      <Routes>
-        <Route path="/" element={<UploadPage />} />
-        <Route path="/report" element={<ReportPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<UploadPage />} />
+      <Route path="/report" element={<ReportPage />} />
+    </Routes>
   );
 }
